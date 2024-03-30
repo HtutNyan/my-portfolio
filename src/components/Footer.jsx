90336abc669088d40ffe4dfe12481526c3cb0571
tharhtutnyan <tharhtutnyan404@gmail.com>
2024-03-30 15:49:17 +0630

    Update Responsive Design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-transparent text-white py-4">
-      <div className="container mx-auto flex justify-center items-center">
+      <div className="container mx-auto flex justify-center items-center flex-wrap">
         {/* Logo 1 */}
-        <img src="https://static-00.iconduck.com/assets.00/behance-icon-2048x1298-jp4p0udd.png" alt="Logo 1" className="w-10 h-auto mx-4" /> 
+        <img src="https://static-00.iconduck.com/assets.00/behance-icon-2048x1298-jp4p0udd.png" alt="Logo 1" className="w-10 h-auto mx-4 my-2" /> 
         {/* Logo 2 */}
-        <img src="https://pngimg.com/d/letter_m_PNG94.png" alt="Logo 2" className="w-10 h-auto mx-4" /> 
+        <img src="https://pngimg.com/d/letter_m_PNG94.png" alt="Logo 2" className="w-10 h-auto mx-4 my-2" /> 
         {/* Logo 3 */}
-        <img src="https://icon-library.com/images/white-globe-icon/white-globe-icon-24.jpg" alt="Logo 3" className="w-10 h-auto mx-4" /> 
+        <img src="https://icon-library.com/images/white-globe-icon/white-globe-icon-24.jpg" alt="Logo 3" className="w-10 h-auto mx-4 my-2" /> 
         {/* Logo 4 */}
-        <img src="https://www.edigitalagency.com.au/wp-content/uploads/Linkedin-logo-white-png-icon-transparent.png" alt="Logo 4" className="w-10 h-auto mx-4" /> 
+        <img src="https://www.edigitalagency.com.au/wp-content/uploads/Linkedin-logo-white-png-icon-transparent.png" alt="Logo 4" className="w-10 h-auto mx-4 my-2" /> 
         {/* Logo 5 */}
-        <img src="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph.png" alt="Logo 5" className="w-10 h-auto mx-4" /> 
+        <img src="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph.png" alt="Logo 5" className="w-10 h-auto mx-4 my-2" /> 
         {/* Logo 6 */}
-        <img src="https://pngimg.com/d/twitter_PNG15.png" alt="Logo 6" className="w-10 h-auto mx-4" /> 
+        <img src="https://pngimg.com/d/twitter_PNG15.png" alt="Logo 6" className="w-10 h-auto mx-4 my-2" /> 
       </div>
     </footer>
   );
